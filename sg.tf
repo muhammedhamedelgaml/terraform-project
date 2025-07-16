cd0ffa8d256e5e959f0163ea6e9bd661b09ef514
muhammedhamedelgaml <muhammedhamedelgaml@gmail.com>
2025-07-16 22:26:04 +0300

    security groups

--- sg.tf
+++ sg.tf
@@ -31,6 +31,12 @@ resource "aws_security_group" "proxy_sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_public_sg.id]
   }
+      ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
@@ -38,6 +44,7 @@ resource "aws_security_group" "proxy_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
 }
 
 resource "aws_security_group" "alb_internal_sg" {
@@ -70,6 +77,21 @@ resource "aws_security_group" "app_sg" {
     to_port         = 5000
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_internal_sg.id]
+  }
+    ingress {
+    description     = "Allow internal lb to access backend (port 80)"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_internal_sg.id]
+  }
+    ingress {
+    description     = "ssh"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.proxy_sg.id]
+
   }
 
   egress {
