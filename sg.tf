e1bacad6efce1b2d0c0a431b1eb4e8dc2e4968ab
muhammedhamedelgaml <muhammedhamedelgaml@gmail.com>
2025-07-16 06:13:19 +0300

    security groups

---
+++ sg.tf
@@ -0,0 +1,81 @@
+# Security Groups
+
+resource "aws_security_group" "alb_public_sg" {
+  name   = "alb-public-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "proxy_sg" {
+  name   = "nginx-proxy-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow traffic from Public ALB on port 80"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_public_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb_internal_sg" {
+  name   = "alb-internal-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow proxy instances to call internal ALB (port 5000)"
+    from_port       = 5000
+    to_port         = 5000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.proxy_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+  name   = "webapp-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow internal lb to access backend (port 5000)"
+    from_port       = 5000
+    to_port         = 5000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_internal_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
