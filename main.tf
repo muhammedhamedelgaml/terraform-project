c292aea81075c0bf77143cd09d3b55e3c033d5c9
muhammedhamedelgaml <muhammedhamedelgaml@gmail.com>
2025-07-17 03:25:31 +0300

    add ec2&vpc module

---
+++ main.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+  source              = "./modules/vpc"
+  vpc_cidr            = "10.0.0.0/16"
+  public_subnets      = ["10.0.0.0/24", "10.0.2.0/24"]
+  private_subnets     = ["10.0.1.0/24", "10.0.3.0/24"]
+  availability_zones  = ["us-east-1a", "us-east-1b"]
+}
+
+
+
+module "ec2" {
+  source               = "./modules/ec2"
+  ami                  = "ami-0150ccaf51ab55a51"
+  instance_type        = "t2.micro"
+  public_subnet_ids    = module.vpc.public_subnet_ids
+  private_subnet_ids   = module.vpc.private_subnet_ids
+  proxy_sg_id          = module.sg.proxy_sg_id
+  app_sg_id            = module.sg.app_sg_id
+  key_name             = "ubuntu"
+  private_key_path     = "/home/muhammed-hamed/Downloads/ubuntu.pem"
+  ssh_user             = "ec2-user"
+
+  nginx_conf           = data.template_file.nginx_conf.rendered
+  frontend_script_path = "./template/startApp_frontend.sh"
+  backend_script_path  = "./template/startApp_backend.sh"
+}
