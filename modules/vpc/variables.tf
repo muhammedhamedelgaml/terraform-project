d51fe38e74a51cc87a8c67f5da22eac480dae86a
muhammedhamedelgaml <muhammedhamedelgaml@gmail.com>
2025-07-17 03:13:03 +0300

    vars of the vpc module

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  default = ["10.0.0.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  default = ["10.0.1.0/24", "10.0.3.0/24"]
+}
+
+variable "availability_zones" {
+  default = ["us-east-1a", "us-east-1b"]
+}
