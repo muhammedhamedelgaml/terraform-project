2aa5213b288fd4a05d8f90cf1149f0cc23088637
muhammedhamedelgaml <muhammedhamedelgaml@gmail.com>
2025-07-17 03:26:01 +0300

    ec2 module

---
+++ modules/ec2/main.tf
@@ -0,0 +1,48 @@
+# EC2 Instances - Proxies
+resource "aws_instance" "proxy" {
+  count                     = 2
+  ami                       = var.ami
+  instance_type             = var.instance_type
+  subnet_id                 = var.public_subnet_ids[count.index]
+  vpc_security_group_ids    = [var.proxy_sg_id]
+  associate_public_ip_address = true
+  key_name                  = var.key_name
+  tags                      = { Name = "nginx-proxy-${count.index + 1}" }
+
+  provisioner "file" {
+    content     = var.nginx_conf
+    destination = "/tmp/nginx.conf"
+  }
+
+  provisioner "file" {
+    source      = var.frontend_script_path
+    destination = "/tmp/startApp_frontend.sh"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = var.ssh_user
+    private_key = file(var.private_key_path)
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/startApp_frontend.sh",
+      "sudo /tmp/startApp_frontend.sh"
+    ]
+  }
+}
+
+
+# EC2 Instances - Web App
+resource "aws_instance" "app" {
+  count                     = 2
+  ami                       = var.ami
+  instance_type             = var.instance_type
+  subnet_id                 = var.private_subnet_ids[count.index]
+  vpc_security_group_ids    = [var.app_sg_id]
+  key_name                  = var.key_name
+  user_data                 = file(var.backend_script_path)
+  tags                      = { Name = "web-app-${count.index + 1}" }
+}
